3c6eeb938cb39f4fc2700600f61ae772371b53d8
andy_yang <tzutse.yang@gmail.com>
2023-02-17 23:15:43 +0800

    close form submit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ const App = () => (
   <div className='w-full'>
     <Home />
     <Info />
-    <Form />
+    <div class='w-full space-y-8 my-auto mx-0 py-20'>
+      <div className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
+        已過表單統計時間，如需變更，請洽新人或主婚人
+      </div>
+    </div>
+    {/* <Form /> */}
   </div>
 );
 
